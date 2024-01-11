8c75feca47ebb34f9bd4b963e7de61c21a993731
dganesh22 <dganesh2022@gmail.com>
2024-01-11 14:09:24 +0530

    ui-components created,signin and register page ui is done

---
+++ src/component/chat/HeaderMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeaderMenu() {
+  return (
+    <div>
+      menu
+    </div>
+  )
+}
+
+export default HeaderMenu
---
+++ src/images/pics.js
@@ -0,0 +1,11 @@
+import loginImage from './login.svg'
+import signUpImage from './register.svg'
+import signUpImage1 from './reg-1.svg'
+
+const imgSrc = {
+    loginImage,
+    signUpImage,
+    signUpImage1
+}
+
+export default imgSrc
